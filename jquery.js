69933a79d8a469432878c0a7c351d0c1465d4fa0
kazimeshkat <66875314+kazimeshkat@users.noreply.github.com>
2020-07-11 23:43:09 +0600

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,17 @@
+// $(document).ready(function () {
+
+//     $('#main li').hover(
+//         function () {
+//             $('.dropdownli', this).slideDown(500)
+//         },
+//         function () {
+//             $('.dropdownli', this).slideUp(500)
+//         }
+//     );
+//     $('.dropdownli').hover(
+//         function () {
+//             $('.dropdownli a', this).css('color','red')
+//         }
+//     );
+// });
+
